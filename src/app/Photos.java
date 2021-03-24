ef280f5e1a426c3ba5dbdee1ace069116a8a7a5d
Jahnavi Bavuluri <jbavuluri@gmail.com>
2021-03-23 22:12:10 -0400

    added packages and files

---
+++ src/app/Photos.java
@@ -0,0 +1,7 @@
+package app;
+
+public class Photos {
+    public static void main(String[] args) {}
+
+
+}
--- src/trial.java
+++
@@ -1,6 +0,0 @@
-public class trial{
-    public static void main(String args[]) {
-        System.out.println("hello world");
-        System.out.println("hello world 2");
-    }
-}
