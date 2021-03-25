bb14fb626017110c3d45f69059d7351d82900344
Jahnavi Bavuluri <jbavuluri@gmail.com>
2021-03-25 02:21:33 -0400

    AlbumController and IndividualAlbumController connected and can programatically add albums to view when button is pressed

--- src/model/AlbumController.java
+++ src/model/AlbumController.java
@@ -1,11 +1,79 @@
 package model;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
 
 public class AlbumController {
+
+    @FXML
+    ScrollPane scroll;
+
+    @FXML
+    GridPane grid;
+
+    public int row = 0;
+    public int col = 0;
 
     public void start(Stage mainStage) {
 
     }
 
+    public void setImage(ActionEvent e) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/IndividualAlbumController.fxml"));
+        try {
+            AnchorPane img = (AnchorPane)loader.load();
+            IndividualAlbumController test = loader.getController();
+            test.album_grid.setVisible(true);
+            Image i = new javafx.scene.image.Image("/view/image.jpg");
+            test.image.setImage(i);
+            grid.add(test.album_grid, col, row);
+            if (col == 2) {
+                row++;
+                col = 0;
+            } else {
+                col++;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
+
+ /*GridPane album_grid = new GridPane();
+        album_grid.addRow(100);
+        album_grid.addRow(33);
+        album_grid.addRow(34);
+        album_grid.addRow(33);
+        album_grid.setPrefHeight(200);
+        album_grid.setPrefWidth(190);
+        ImageView imgView = new ImageView();
+        imgView.setFitWidth(100);
+        imgView.setFitHeight(100);
+        Image i = new javafx.scene.image.Image("/view/image.jpg");
+        imgView.setImage(i);
+        album_grid.add(imgView, 0,0);
+        Label name = new Label();
+        name.setText("Name of album");
+        album_grid.add(name,0,1);
+        Label number = new Label();
+        number.setText("number of pictures in album");
+        album_grid.add(number,0,2);
+        Label date = new Label();
+        date.setText("Date of album");
+        album_grid.add(date,0,3);
+        grid.add(album_grid, col, row);
+        if (col == 2) {
+            row++;
+            col = 0;
+        } else {
+            col++;
+        }*/
---
+++ src/model/IndividualAlbumController.java
@@ -0,0 +1,17 @@
+package model;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class IndividualAlbumController {
+
+    @FXML
+    ImageView image;
+
+    @FXML
+    GridPane album_grid;
+
+}
