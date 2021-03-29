8f2b7ce83b744f72fc5fb2947a2c56fc3dd9e45b
Chiraag Rekhari <crekhari@gmail.com>
2021-03-29 01:04:35 -0400

    admincontroller

--- src/model/AdminController.java
+++ src/model/AdminController.java
@@ -1,15 +1,49 @@
 package model;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+import java.util.ArrayList;
+
 
 public class AdminController {
+
+    @FXML TextField add;
+    @FXML TextField delete;
+    //ArrayList<User> UsersList = new ArrayList<User>();
+
     public void start(Stage mainstage) {
 
     }
+/*
+    public void add(ActionEvent e){
+        UsersList.add(new User(add.getText().trim()));
+        print();
+    }
+
+    public void delete(ActionEvent e){
+        for(int i = 0; i < UsersList.size(); i++) {
+            if((UsersList.get(i).username).equals(delete.getText().trim())) {
+                UsersList.remove(i);
+                i--;
+            }
+        }
+        print();
+    }
+
+    public void logout(ActionEvent e){
+
+    }
+
+    public ArrayList<User> getUsers(){
+        return UsersList;
+    }
+
+    public void print(){
+        for(int i = 0; i < UsersList.size(); i++) {
+            System.out.print(User.get(i).username);
+        }
+    }
+    */
 }
