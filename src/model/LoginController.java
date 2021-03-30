422fc92808cc47d82eb2789eabc4078e5a7914b7
Chiraag Rekhari <crekhari@gmail.com>
2021-03-30 00:28:37 -0400

    loginController - login method method works for admin

--- src/model/LoginController.java
+++ src/model/LoginController.java
@@ -28,29 +28,30 @@ public class LoginController {
 
     @FXML TextField login;
     @FXML Button login_btn;
-    Stage mainStage;
+
     public void start(Stage mainStage) {
-    this.mainStage = mainStage;
     }
 
     public void login(ActionEvent e) throws Exception{
 
 
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/view/admin.fxml"));
-        try {
-            mainStage.close();
-            AnchorPane root = (AnchorPane)loader.load();
-            AdminController adminview = loader.getController();
-            Stage stage = new Stage();
+        if((login.getText().toLowerCase()).equals("admin")) {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/view/admin.fxml"));
+            try {
 
-            adminview.start(stage);
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+                AnchorPane root = (AnchorPane) loader.load();
+                AdminController adminview = loader.getController();
+                Stage stage = new Stage();
 
-        } catch (IOException exception) {
-            exception.printStackTrace();
+                adminview.start(stage);
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
         }
 
             /*Stage appStage;
